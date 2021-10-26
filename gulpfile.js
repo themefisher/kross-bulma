1c9aa68615f988524eccc37b99034220cb9d59da
somratpro <abdulmonnafsomrat@gmail.com>
2021-10-26 13:40:16 +0600

    remove bwnista font

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,43 @@
 'use strict';
 
-var gulp         = require('gulp');
-var sass         = require('gulp-sass');
-var gutil        = require('gulp-util');
-var jshint       = require('gulp-jshint');
-var sourcemaps   = require('gulp-sourcemaps');
-var uglify       = require('gulp-uglify');
-var stylish      = require('jshint-stylish');
-var plumber      = require('gulp-plumber');
-var notify       = require('gulp-notify');
-var fileinclude  = require('gulp-file-include');
-var autoprefixer = require('gulp-autoprefixer');
-var runSequence  = require('run-sequence');
-var zip          = require('gulp-zip');
-var bs           = require('browser-sync').create();
-var rimraf       = require('rimraf'); 
-var gm 			     = require('gulp-gm');
+const sass = require('gulp-sass')(require('sass'));
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const fileinclude = require('gulp-file-include');
+const autoprefixer = require('gulp-autoprefixer');
+const bs = require('browser-sync').create();
+const rimraf = require('rimraf');
+const comments = require('gulp-header-comment');
 
 var path = {
-    src: { 
-        // source
-        html    : 'src/*.html',
-        others  : 'src/*.+(php|ico|png)',
-        htminc  : 'src/partials/**/*.htm',
-        incdir  : 'src/partials/',
-        plugins : 'src/plugins/**/*.*',
-        js      : 'src/js/*.js',
-        scss    : 'src/scss/**/*.scss',
-        images  : 'src/images/**/*.+(png|jpg|gif|svg)',
-        doc     : 'documentation/**/*.*',
-        blur 	  : 'src/images/**/*.jpg'
-    },
-    build: { 
-        dirDev        : 'public/template/'
-    }
-};
-
-var template = {
-  version: {
-    free: 'free',
+  src: {
+    html: 'source/*.html',
+    others: 'source/*.+(php|ico|png)',
+    htminc: 'source/partials/**/*.htm',
+    incdir: 'source/partials/',
+    plugins: 'source/plugins/**/*.*',
+    js: 'source/js/*.js',
+    scss: 'source/scss/**/*.scss',
+    images: 'source/images/**/*.+(png|jpg|gif|svg)'
+  },
+  build: {
+    dirNetlify: 'netlify/',
+    dirDev: 'theme/'
   }
 };
-
-/* =====================================================
-Development Builds
-===================================================== */
 
 // HTML
 gulp.task('html:build', function () {
   return gulp.src(path.src.html)
-    .pipe(customPlumber('Error Running html-include'))
     .pipe(fileinclude({
-      basepath: path.src.incdir, context: {
-        version: template.version.free
-      }
+      basepath: path.src.incdir
     }))
+    .pipe(comments(`
+    WEBSITE: https://themefisher.com
+    TWITTER: https://twitter.com/themefisher
+    FACEBOOK: https://www.facebook.com/themefisher
+    GITHUB: https://github.com/themefisher/
+    `))
     .pipe(gulp.dest(path.build.dirDev))
     .pipe(bs.reload({
       stream: true
@@ -63,13 +46,19 @@ gulp.task('html:build', function () {
 
 // SCSS
 gulp.task('scss:build', function () {
-  var ignoreNotification = false;
   return gulp.src(path.src.scss)
-    .pipe(customPlumber('Error Running Sass'))
     .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
     .pipe(autoprefixer())
-    .pipe(sourcemaps.write('/maps'))
+    .pipe(sourcemaps.write('/'))
+    .pipe(comments(`
+    WEBSITE: https://themefisher.com
+    TWITTER: https://twitter.com/themefisher
+    FACEBOOK: https://www.facebook.com/themefisher
+    GITHUB: https://github.com/themefisher/
+    `))
     .pipe(gulp.dest(path.build.dirDev + 'css/'))
     .pipe(bs.reload({
       stream: true
@@ -79,15 +68,12 @@ gulp.task('scss:build', function () {
 // Javascript
 gulp.task('js:build', function () {
   return gulp.src(path.src.js)
-    .pipe(customPlumber('Error Running JS'))
-    .pipe(jshint('./.jshintrc'))
-    .pipe(notify(function (file) {
-      if (!file.jshint.success) {
-        return file.relative + " (" + file.jshint.results.length + " errors)\n";
-      }
-    }))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .on('error', gutil.log)
+    .pipe(comments(`
+  WEBSITE: https://themefisher.com
+  TWITTER: https://twitter.com/themefisher
+  FACEBOOK: https://www.facebook.com/themefisher
+  GITHUB: https://github.com/themefisher/
+  `))
     .pipe(gulp.dest(path.build.dirDev + 'js/'))
     .pipe(bs.reload({
       stream: true
@@ -112,49 +98,37 @@ gulp.task('plugins:build', function () {
     }));
 });
 
-// Other files like favicon, php, apple-icon on root directory
+// Other files like favicon, php, sourcele-icon on root directory
 gulp.task('others:build', function () {
   return gulp.src(path.src.others)
     .pipe(gulp.dest(path.build.dirDev))
 });
 
-// Error Message Show
-function customPlumber(errTitle) {
-  return plumber({
-    errorHandler: notify.onError({
-      // Customizing error title
-      title: errTitle || "Error running Gulp",
-      message: "Error: <%= error.message %>",
-      sound: "Glass"
-    })
-  });
-}
-
 // Clean Build Folder
 gulp.task('clean', function (cb) {
-  rimraf('./builds', cb);
+  rimraf('./theme', cb);
 });
 
 // Watch Task
 gulp.task('watch:build', function () {
-  gulp.watch(path.src.html, ['html:build']);
-  gulp.watch(path.src.htminc, ['html:build']);
-  gulp.watch(path.src.scss, ['scss:build']);
-  gulp.watch(path.src.js, ['js:build']);
-  gulp.watch(path.src.images, ['images:build']);
-  gulp.watch(path.src.plugins, ['plugins:build']);
+  gulp.watch(path.src.html, gulp.series('html:build'));
+  gulp.watch(path.src.htminc, gulp.series('html:build'));
+  gulp.watch(path.src.scss, gulp.series('scss:build'));
+  gulp.watch(path.src.js, gulp.series('js:build'));
+  gulp.watch(path.src.images, gulp.series('images:build'));
+  gulp.watch(path.src.plugins, gulp.series('plugins:build'));
 });
 
 // Build Task
-gulp.task('build', function () {
-  runSequence(
-    'clean',
-    'html:build',
-    'js:build',
-    'scss:build',
-    'images:build',
-    'plugins:build',
-    'others:build',
+gulp.task('default', gulp.series(
+  'clean',
+  'html:build',
+  'js:build',
+  'scss:build',
+  'images:build',
+  'plugins:build',
+  'others:build',
+  gulp.parallel(
     'watch:build',
     function () {
       bs.init({
@@ -162,100 +136,64 @@ gulp.task('build', function () {
           baseDir: path.build.dirDev,
         }
       });
-    }
-  );
-});
+    })
+  )
+);
 
-gulp.task("default", ["build"]);
 
 /* =====================================================
-Free Builds
+Netlify Builds
 ===================================================== */
 // HTML
-gulp.task('html:free:build', function() {
-  return gulp.src( path.src.html )
-    .pipe(customPlumber('Error Running html-include'))
-    .pipe(fileinclude({ basepath: path.src.incdir, context: {
-      version: template.version.free
-    } }))
-    .pipe(gulp.dest( path.build.dir ));
+gulp.task('html:netlify:build', function () {
+  return gulp.src(path.src.html)
+    .pipe(fileinclude({
+      basepath: path.src.incdir
+    }))
+    .pipe(gulp.dest(path.build.dirNetlify));
 });
 
 // SCSS
-gulp.task('scss:free:build', function () {
-  var ignoreNotification = false;
+gulp.task('scss:netlify:build', function () {
   return gulp.src(path.src.scss)
-    .pipe(customPlumber('Error Running Sass'))
     .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(sourcemaps.write('/maps'))
-    .pipe(gulp.dest(path.build.dir + 'css/'));
+    .pipe(gulp.dest(path.build.dirNetlify + 'css/'));
 });
 
 // Javascript
-gulp.task('js:free:build', function () {
+gulp.task('js:netlify:build', function () {
   return gulp.src(path.src.js)
-    .pipe(customPlumber('Error Running JS'))
-    .pipe(jshint('./.jshintrc'))
-    .pipe(notify(function (file) {
-      if (!file.jshint.success) {
-        return file.relative + " (" + file.jshint.results.length + " errors)\n";
-      }
-    }))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .on('error', gutil.log)
-    .pipe(gulp.dest(path.build.dir + 'js/'));
+    .pipe(gulp.dest(path.build.dirNetlify + 'js/'));
 });
 
 // Images
-gulp.task('images-blur:free:build', function () {
-  return gulp.src(path.src.blur)
-    .pipe(gm(function (gmfile) {
-      return gmfile.blur(30, 30);
-    }))
-    .pipe(gulp.dest(path.build.dir + 'images/'))
-    .pipe(bs.reload({
-      stream: true
-    }));
-});
-
-gulp.task('images:free:build', function () {
+gulp.task('images:netlify:build', function () {
   return gulp.src(path.src.images)
-    .pipe(gulp.dest(path.build.dir + 'images/'));
+    .pipe(gulp.dest(path.build.dirNetlify + 'images/'));
 });
 
 // Plugins
-gulp.task('plugins:free:build', function () {
+gulp.task('plugins:netlify:build', function () {
   return gulp.src(path.src.plugins)
-    .pipe(gulp.dest(path.build.dir + 'plugins/'))
+    .pipe(gulp.dest(path.build.dirNetlify + 'plugins/'))
 });
 
 // Other files like favicon, php, apple-icon on root directory
-gulp.task('others:free:build', function () {
+gulp.task('others:netlify:build', function () {
   return gulp.src(path.src.others)
-    .pipe(gulp.dest(path.build.dir))
-});
-
-// Zipping
-gulp.task('free-zip', function () {
-  return gulp.src(path.build.dir + '/**/*.*')
-    .pipe(zip('free.zip'))
-    .pipe(gulp.dest('./builds'))
+    .pipe(gulp.dest(path.build.dirNetlify))
 });
 
 // Build Task
-gulp.task('free', function () {
-  runSequence(
-    'html:free:build',
-    'js:free:build',
-    'scss:free:build',
-    'images:free:build',
-    'images-blur:free:build',
-    'plugins:free:build',
-    'free-zip'
-  );
-});
-
-
-
+gulp.task('netlify', gulp.series(
+  'html:netlify:build',
+  'js:netlify:build',
+  'scss:netlify:build',
+  'images:netlify:build',
+  'plugins:netlify:build'
+));
